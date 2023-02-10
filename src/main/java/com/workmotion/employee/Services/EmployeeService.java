7451513739ce4eff544df874e89bb6004b1d4351
Amr Khaled <amrkh97@gmail.com>
2023-02-10 14:11:38 +0200

    Changes: - Added Interface for EmployeeService - Refactoring the code to the new implementation

--- src/main/java/com/workmotion/employee/Services/EmployeeService.java
+++ src/main/java/com/workmotion/employee/Services/EmployeeService.java
@@ -1,47 +1,13 @@
 package com.workmotion.employee.Services;
 
-import com.github.oxo42.stateless4j.StateMachine;
 import com.workmotion.employee.Models.Employee;
 import com.workmotion.employee.Models.EmployeeEvent;
-import com.workmotion.employee.Models.EmployeeState;
-import com.workmotion.employee.Repositories.EmployeeRepository;
-import com.workmotion.employee.StateMachine.EmployeeStateConfig;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+public interface EmployeeService {
 
-@Service
-@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
-public class EmployeeService {
+    Employee changeState(Employee employee, EmployeeEvent event);
 
-    private final EmployeeRepository employeeRepository;
+    Employee addEmployee(Employee employee);
 
-    public Employee changeState(Employee employee, EmployeeEvent event) {
-
-        StateMachine<EmployeeState, EmployeeEvent> stateMachine = new StateMachine<>(employee.getState(),
-                EmployeeStateConfig.build());
-        try {
-            stateMachine.fire(event);
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-
-        if (!employee.getState().equals(stateMachine.getState())) {
-            employee.setState(stateMachine.getState());
-            employeeRepository.save(employee);
-
-        }
-        return employee;
-    }
-
-    public Employee addEmployee(Employee employee) {
-        return this.employeeRepository.save(employee);
-    }
-
-    public Employee getEmployeeById(Long id) {
-        Optional<Employee> employee = this.employeeRepository.findById(id);
-        return employee.orElseThrow(() -> new RuntimeException("No Employee found with provided id"));
-    }
+    Employee getEmployeeById(Long id);
 }
---
+++ src/main/java/com/workmotion/employee/Services/EmployeeServiceImpl.java
@@ -0,0 +1,50 @@
+package com.workmotion.employee.Services;
+
+import com.github.oxo42.stateless4j.StateMachine;
+import com.workmotion.employee.Models.Employee;
+import com.workmotion.employee.Models.EmployeeEvent;
+import com.workmotion.employee.Models.EmployeeState;
+import com.workmotion.employee.Repositories.EmployeeRepository;
+import com.workmotion.employee.StateMachine.EmployeeStateConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee changeState(Employee employee, EmployeeEvent event) {
+
+        StateMachine<EmployeeState, EmployeeEvent> stateMachine = new StateMachine<>(employee.getState(),
+                EmployeeStateConfig.build());
+        try {
+            stateMachine.fire(event);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        if (!employee.getState().equals(stateMachine.getState())) {
+            employee.setState(stateMachine.getState());
+            employeeRepository.save(employee);
+
+        }
+        return employee;
+    }
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        return this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        Optional<Employee> employee = this.employeeRepository.findById(id);
+        return employee.orElseThrow(() -> new RuntimeException("No Employee found with provided id"));
+    }
+}
